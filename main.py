4895ad154c7727103768ff846d2d029574fff534
CheM1C@LaRt <chemical.art@gmail.com>
2024-03-23 18:48:24 +0300

    finish

--- main.py
+++ main.py
@@ -19,18 +19,33 @@ target_x = random.randint(0, SCREEN_WIDTH - target_width)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
 background = pygame.image.load("img/back.png")
+background = pygame.transform.scale(background, (SCREEN_WIDTH, SCREEN_HEIGHT))
 
+catch = pygame.image.load("img/catch.png")
 running = True
+show_message = False
+message_display_time = 0  # Таймер для отображения сообщения
+
 while running:
-    screen.blit = (background, (0, 0))
+    screen.blit(background, (0, 0))
+    time_now = pygame.time.get_ticks()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
             if target_x <= mouse_x <= target_x + target_width and target_y <= mouse_y <= target_y + target_height:
-                print("ПОЙМАЛСЯ!")
                 target_x = random.randint(0, SCREEN_WIDTH - target_width)
                 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+                show_message = True
+                message_display_time = time_now + 500  # Отображать сообщение в течение 500 мс
+    if show_message:
+        if time_now <= message_display_time:
+            screen.blit(catch, (300, 200))
+            #pygame.time.delay(100)
+        else:
+            show_message = False
+    pygame.time.wait(200)
     screen.blit(target_img, (target_x, target_y))
     pygame.display.update()
 
