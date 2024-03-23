014405c3afec443872d1b7d67e03267e464598ee
CheM1C@LaRt <chemical.art@gmail.com>
2024-03-23 16:54:32 +0300

    cycle

--- main.py
+++ main.py
@@ -8,17 +8,30 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption("Catch me if you can!")
-icon = pygame.image.load("icon.png")
+icon = pygame.image.load("img/icon.png")
 pygame.display.set_icon(icon)
 
-target_img = pygame.image.load("target.png")
+target_img = pygame.image.load("img/target.png")
 target_width = 80
 target_height = 80
 
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+
+background = pygame.image.load("img/back.png")
+
 running = True
 while running:
-    pass
+    screen.blit = (background, (0, 0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if target_x <= mouse_x <= target_x + target_width and target_y <= mouse_y <= target_y + target_height:
+                print("ПОЙМАЛСЯ!")
+                target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+    screen.blit(target_img, (target_x, target_y))
+    pygame.display.update()
 
 pygame.quit()
